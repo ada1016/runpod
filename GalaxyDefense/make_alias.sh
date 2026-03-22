fc2fa2cd06858374d07737364c1c08fb002cb971
ada1016 <ada1016@gmail.com>
2026-03-22 22:15:45 +0800

    update scripts
    Signed-off-by: ada1016 <ada1016@gmail.com>

--- GalaxyDefense/make_alias.sh
+++ GalaxyDefense/make_alias.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+
+
 # 1. Define Aliases
-# Using 'alias' allows you to type 'hook' or 'auto' manually in the terminal later
-alias hook='cd ~/Documents/runpod/GalaxyDefense && PID=$(pgrep -n "GalaxyDefense") && lldb -p $PID --batch -o "process detach" -o "quit" && sleep 1 && frida -p $PID -l god.js'
-alias auto='cd ~/Documents/runpod/GalaxyDefense/autoclicker && python3 ./play.py'
+#alias hook='cd ~/Documents/runpod/GalaxyDefense && PID=$(pgrep -n "GalaxyDefense") && lldb -p $PID --batch -o "process detach" -o "quit" && sleep 1 && frida -p $PID -l god.js'
+#alias auto='cd ~/Documents/runpod/GalaxyDefense/autoclicker && python3 ./play.py'
 
 # 2. Display Menu
 echo "------------------------------------------"
@@ -20,15 +21,14 @@ read -p "Select an option [1-3]: " choice
 case $choice in
     1)
         echo "Executing Hook..."
-        # Running the command directly since aliases don't always expand inside the script that defines them
-        hook
+        cd ~/Documents/runpod/GalaxyDefense && PID=$(pgrep -n "GalaxyDefense") && lldb -p $PID --batch -o "process detach" -o "quit" && sleep 1 && frida -p $PID -l god.js
         ;;
     2)
         echo "Executing Auto..."
-        auto
+        cd ~/Documents/runpod/GalaxyDefense/autoclicker && python3 ./play.py
         ;;
     3)
-        echo "Exiting. You can still use 'hook' or 'auto' manually."
+        echo "Exiting. Aliases 'hook' and 'auto' are ready."
         ;;
     *)
         echo "Invalid selection."
