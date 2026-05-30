9e9c6bda10e13583e985510a1fb9f3991abc1e06
ada1016 <ada1016@gmail.com>
2026-05-30 22:15:27 +0800

    remove API key and code
    Signed-off-by: ada1016 <ada1016@gmail.com>

--- api_Wan2.2_LoRA_LTX2/run_api.sh
+++
@@ -1,8 +0,0 @@
-# 1. 建立虛擬環境資料夾 (名稱叫 venv)
-python3 -m venv venv
-
-# 2. 啟動虛擬環境
-source venv/bin/activate
-pip install --upgrade pip  # 建議先升級 pip
-pip install -r requirements.txt
-python run_api.py f01vlwxnwwqsp4 rpa_G9Q9KKTZT2Z8256M8LN76WT4E9ONUCVA0LJ0FHHU569s66
