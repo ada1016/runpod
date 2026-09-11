a10dc71bb0bb27b5baa7a1567ddb57a63e952a56
ada1016 <ada1016@gmail.com>
2026-09-11 14:18:58 +0800

    update git sh

--- tab5/push_to_git.sh
+++ tab5/push_to_git.sh
@@ -5,16 +5,16 @@ GIT_ROOT="/Users/mingdajiang/Documents/runpod"
 
 cd "$GIT_ROOT"
 
-echo "=== Current Git status ==="
-git status --short
+echo "=== Current Tab5 Git status ==="
+git status --short -- tab5/
 
 echo
 echo "=== Adding everything under tab5/ ==="
 git add -A tab5/
 
 echo
-echo "=== Changes to be committed ==="
-git status --short
+echo "=== Tab5 changes to be committed ==="
+git status --short -- tab5/
 
 # Stop if there is actually nothing to commit
 if git diff --cached --quiet; then
